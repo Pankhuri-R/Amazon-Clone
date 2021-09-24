d6aae83e07c266d364bd7d92e6a6159ab29fcb71
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-09-24 13:15:26 +0530

    Header complete

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function Header() {
   return (
@@ -10,6 +12,7 @@ function Header() {
       />
       <div className="header__search">
         <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
         <div className="header__option">
@@ -23,6 +26,10 @@ function Header() {
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
           <span className="header__optionLineTwo">Prime</span>
+        </div>
+        <div className="header__optionBasket">
+          <ShoppingCartIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>

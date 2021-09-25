b5d8b0d894f1ec1b9f6eaa4e4751d796ac0c79ce
Pankhuri-R <63069103+Pankhuri-R@users.noreply.github.com>
2021-09-25 17:45:33 +0530

    Product component complete

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -11,15 +12,48 @@ function Home() {
           alt=""
         />
         <div className="home__row">
-          {/* Product */}
-          {/* Product */}
+          <Product
+            title="The lean startup: A book to inspire you for your future ideas"
+            price={29.3}
+            image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg"
+            rating={5}
+          />
+          <Product
+            title="DVD Player for TV, HD DVD Player with HDMI & AV Cable for Projector "
+            price={35.99}
+            image="https://m.media-amazon.com/images/I/61Mie8CpqsL._AC_UL480_FMwebp_QL65_.jpg"
+            rating={4}
+          />
         </div>
         <div className="home__row">
-          {/* Product */}
-          {/* Product */}
-          {/* Product */}
+          <Product
+            title="Black Face Mask 100pcs Disposable Masks Breathable 3 Layer"
+            price={16}
+            image="https://images-na.ssl-images-amazon.com/images/I/51axS4O5BML.jpg"
+            rating={4}
+          />
+          <Product
+            title="Seagate Portable 2TB External Hard Drive Portable HDD"
+            price={200}
+            image="https://images-na.ssl-images-amazon.com/images/I/31jBba7+ySL.jpg"
+            rating={4}
+          />
+          <Product
+            title="Apple AirPods with Charging Case
+          by Apple"
+            price={299}
+            image="https://images-na.ssl-images-amazon.com/images/I/31jBnwWr91L.jpg"
+            rating={5}
+          />
         </div>
-        <div className="home__row">{/* Product */}</div>
+        <div className="home__row">
+          <Product
+            title="Sony X91J 85 Inch TV: Full Array LED 4K Ultra HD Smart Google TV with Dolby Vision HDR and Alexa Compatibility KD85X91J- 2021 Model, Black"
+            price={2798}
+            image="https://m.media-amazon.com/images/I/91Erri4enJL._AC_UL480_FMwebp_QL65_.jpg"
+            rating={4}
+          />
+        </div>
       </div>
     </div>
   );
